f2deff105686e3a236f661e5caa7ab02b2e83a09
Niravkadia <niravrajput0008@gmail.com>
2022-03-08 10:08:15 +0530

    models commit

--- post/models.py
+++ post/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-
-
 from django.contrib.auth.models import User
 from datetime import datetime
 
@@ -12,10 +10,18 @@ from datetime import datetime
 class Post(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     last_update=models.DateTimeField(auto_now_add=True,blank=True)
-    desciption=models.TextField()
-    
+    description=models.TextField()
 
 
-class Comment(models.Model):
-    user_comment=models.ForeignKey(Post,on_delete=models.CASCADE)
+class Comments(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
     comment_given=models.TextField()
+
+
+class likes(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    comment=models.ForeignKey(Comments,on_delete=models.CASCADE)
+
+
+
+
--- post/views.py
+++ post/views.py
@@ -1,9 +1,11 @@
 from fnmatch import fnmatchcase
+from xml.etree.ElementTree import Comment
 from django.http import HttpResponse
 from django.shortcuts import redirect, render,HttpResponse
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post ,Comments
 from django.contrib import auth
+
 # Create your views here.
 
 def authenticate_user(request):
@@ -20,8 +22,10 @@ def authenticate_user(request):
         return HttpResponse("your password is incorrect")
 
 
+
 def login(request):
     return render(request,'login.html')
+
 
 def registration(request):
     return render(request,'registration.html')
@@ -33,7 +37,6 @@ def add_user(request):
         email=request.POST['email']
         pass1=request.POST['pass1']
         myuser=User.objects.create_user(username,email,pass1)
-
         myuser.save()
         return redirect('/')
     else:
@@ -50,19 +53,18 @@ def show(request):
 def compose(request):
     return render(request,'compose.html')
 
-
-
 def Added_post(request):    
     if request.method=="POST":
         vsr=request.user.id
         description=request.POST['txtArea']
-        data_store=Post(user_id=vsr,desciption=description)
+        data_store=Post(user_id=vsr,description=description)
         data_store.save()
         
         return redirect('/Added_post')
     else:
         all_posts=Post.objects.all().order_by('-id')
         return render(request,"Added_post.html",{'all_posts':all_posts})
+        
 
        
 def back(request):
@@ -71,13 +73,27 @@ def back(request):
 
 def post_delete(request,id):
     user_delete=Post.objects.filter(id=id)
-    
     user_delete.delete()
+    
     return redirect('/Added_post')
 
 
-def pass_comment(request,id):
-    user_comments=Post.objects.all(id=id)
-    return redirect(request,'Added_post.html',{'user_comments':user_comments})
+def pass_comment(request):
+    return render(request,'pass_comment.html')
+
+def to_save_comment(request):
+    if request.method=='POST':
+        user_id=request.user.id
+        print(user_id)
+        comment_given=request.POST['comment_txtArea']
+        print(comment_given)
+        
+        data_store=Comments(post__User_id=user_id,desciption=comment_given)
+        print(data_store)
+        data_store.save()
+        return HttpResponse("your data stored in database successfully")
+    else:
+        return HttpResponse("your comment data saved")
+
 
 
